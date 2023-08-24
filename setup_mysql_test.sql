b4fdfa636c2d70b261f441179963791dec9a309e
AliMousa98 <ali.abdelaal1998@gmail.com>
2023-08-23 17:06:21 -0700

    setup_mysql_test.sql -2

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
--- Creata DB and user for test
+-- Create test database and user
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
-GRANT ALL ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
